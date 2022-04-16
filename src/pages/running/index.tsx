c32a7dced3a60dd9ca8fd1bb8195c80d30d002d7
Samuel Morrissey <samuel.morrissey@outlook.com>
2022-04-16 13:55:30 -0300

    Design do primeiro painel

--- src/pages/running/index.tsx
+++ src/pages/running/index.tsx
@@ -3,25 +3,36 @@ import { FaArrowUp } from "react-icons/fa";
 export default () => {
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <h1>Running</h1>
       <div className="flex gap-2">
-        <div className="flex justify-center items-center gap-2">
-          <span>25:00min</span>
-          <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-1 border rounded p-2">
+          <span>Tempo total</span>
+          <div className="flex justify-center items-end gap-1">
+            <span className="text-4xl">25:00</span>
+            <span>min</span>
+          </div>
+          <div className="flex justify-center items-center text-success">
             <FaArrowUp />
             <span>12%</span>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-2">
-          <span>3.00km</span>
-          <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-1 border rounded p-2">
+          <span>Distância</span>
+          <div className="flex justify-center items-end gap-1">
+            <span className="text-4xl">3.00</span>
+            <span>km</span>
+          </div>
+          <div className="flex justify-center items-center text-success">
             <FaArrowUp />
             <span>12%</span>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-2">
-          <span>{(25 / 3).toFixed(2)}min/km</span>
-          <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-1 border rounded p-2">
+          <span>Ritmo</span>
+          <div className="flex justify-center items-end gap-1">
+            <span className="text-4xl">{(25 / 3).toFixed(2)}</span>
+            <span>min/km</span>
+          </div>
+          <div className="flex justify-center items-center text-success">
             <FaArrowUp />
             <span>12%</span>
           </div>
